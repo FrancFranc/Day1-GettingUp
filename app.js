8b8668a012afab0ecd1caccf56a384510a24862e
Evette Cheung <FrancFranc@gmx.com>
2017-04-05 13:21:14 -0700

    Assignment Done.

---
+++ app.js
@@ -0,0 +1,16 @@
+var supermarket = prompt('What groceries do you want to get?');
+alert('Well, we should get some '+ supermarket);
+
+var food = prompt('Snacks?  Meat?  Beer?  Or other thing else?');
+alert('Sounds good.  How about we get more '+ food);
+
+var fruits = prompt('Do you want some fruits?');
+alert(fruits + "Let's get a few!");
+
+var all = confirm("Do we have enough for tonight's party?");
+alert("Should be fine, so, right now we've got" + supermarket + "including" + food + "and" + fruits + ".");
+
+console.log(supermarket);
+console.log(food);
+console.log(fruits);
+console.log(all);
